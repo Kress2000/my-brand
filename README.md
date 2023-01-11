f08f2fb65539178e478f5ca2ab613afcb8059121
Kress2000 <erickykress1@gmail.com>
2023-01-11 18:51:08 +0200

    my folio pages

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nsanzimfura-Folio.github.io
+This is my website being built by only HTML5, CSS3 AND JS. It is made by such stacks as suggested by [@andela](https://github.com/andela).
+
+Made by [Nsanzimfura Eric](github.com/Kress20000).
+
+  © 2023. All rights reserved.
---
+++ index.js
@@ -0,0 +1,114 @@
+// constants 
+const navBar = document.getElementById("nav-section");
+const navLinks = document.querySelectorAll(".nav-links");
+const navATags = document.querySelectorAll(".nav-links  a");
+const smallNav = document.getElementById("small-nav");
+const copyRightYear = document.querySelectorAll(".year");
+// btns
+const hambaga = document.getElementById("hambaga");
+const closeBtn = document.getElementById("close-box");
+// cursor
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+// navbar hover on a tags
+const navbarHover = ()=>{
+    navLinks.forEach((li)=>{
+        navATags.forEach(aTag=>{
+            aTag.style.color = "white";
+     })
+        li.addEventListener("mouseover", ()=>{
+            navATags.forEach(aTag=>{
+                aTag.classList.remove("purpledcolor");
+                aTag.style.color = "hsla(0, 0%, 100%, .3";
+             })
+         })
+         li.addEventListener("mouseout", ()=>{
+             navATags.forEach(aTag=>{
+                 aTag.style.color = "white";
+          })
+     })
+     })
+     navATags.forEach(hoveredTag =>{
+         hoveredTag.addEventListener("mouseover", ()=>{
+             hoveredTag.setAttribute("id", "hovered");
+         })
+         hoveredTag.addEventListener("mouseout", ()=>{
+             hoveredTag.removeAttribute("id");
+         })
+     })
+}
+navbarHover();
+const navBarHoverScrolling = ()=>{
+    const stictyNavLinks = document.querySelectorAll(".sticky .nav-links");
+    const stickyNavATags = document.querySelectorAll(".sticky .nav-links li a");
+    stictyNavLinks.forEach((li)=>{
+        li.addEventListener("mouseover", ()=>{
+            stickyNavATags.forEach(aTag=>{
+                aTag.classList.add("purpledcolor");
+             })
+         })
+         li.addEventListener("mouseout", ()=>{
+            stickyNavATags.forEach(aTag=>{
+                aTag.classList.remove("purpledcolor");
+                 aTag.style.color = "#A40FFF";
+          })
+     })
+     })
+     stickyNavATags.forEach(hoveredTag =>{
+         hoveredTag.addEventListener("mouseover", ()=>{
+             hoveredTag.setAttribute("id", "hoveredSticky");
+         })
+         hoveredTag.addEventListener("mouseout", ()=>{
+             hoveredTag.removeAttribute("id");
+         })
+     })
+}
+// sticky navbar bottom
+let sticky = navBar.offsetTop;
+window.addEventListener("scroll", ()=>{
+    const countScroll = window.innerHeight + window.scrollY;
+    const countOffset= document.body.offsetHeight;
+    // console.log("scroll: ", countScroll, "Offset: ", countOffset, "Top: ", sticky, "Compare: ", window.scrollY )
+    if (window.scrollY >= sticky) {    
+        navBar.classList.add("sticky");
+        navBarHoverScrolling();
+    }
+    else if(window.scrollY < sticky || countScroll < countOffset ) {
+        navBar.classList.remove("sticky");  
+        navBar.classList.remove("stickyTop");  
+        navbarHover();
+    }
+    if(countScroll >= countOffset){
+        navBar.classList.remove("sticky");  
+        navBar.classList.add("stickyTop");  
+        navBarHoverScrolling();
+    }
+});
+// Min screens navbar
+hambaga.addEventListener("click", ()=>{
+    smallNav.style.display = "none";
+    navBar.style.display = "flex";
+});
+closeBtn.addEventListener("click", ()=>{
+    smallNav.style.display = "flex";
+    navBar.style.display = "none";
+})
+// copy right years
+copyRightYear.forEach(year=>{
+    const time = new Date()
+    const timeYear = time.getFullYear();
+    year.innerText= timeYear;
+})
+
+
+
+
