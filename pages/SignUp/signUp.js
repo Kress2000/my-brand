a228e4325329f7a4a8c86d467d290f186043c89a
Kress2000 <erickykress1@gmail.com>
2023-01-13 05:34:21 +0200

    blogs

--- pages/SignUp/signUp.js
+++ pages/SignUp/signUp.js
@@ -5,3 +5,15 @@ copyRightYear.forEach(year=>{
     const timeYear = time.getFullYear();
     year.innerText= timeYear;
 })
+// cursor
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
