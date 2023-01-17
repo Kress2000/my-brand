7460df23a9de31fb3e8edd72b03775f80a1f22d0
Kress2000 <erickykress1@gmail.com>
2023-01-17 02:53:20 +0200

    Functionality of signUp/SignIn (form validation)

--- pages/SignIn/signIn.js
+++ pages/SignIn/signIn.js
@@ -1,4 +1,14 @@
 const copyRightYear = document.querySelectorAll(".year");
+const submitSignInData = document.getElementById("submitSignUpData");
+const alertMessage = document.getElementById("alertMessage");
+const nameUser = document.getElementById("name")
+const email = document.getElementById("email")
+const passcode = document.getElementById("passcode")
+const getFormData = JSON.parse(localStorage.getItem("SignedInSuccessfully"));
+// changing Sign in input data
+email.value = getFormData.email;
+passcode.value = getFormData.passcode;
+nameUser.value = getFormData.name;
 
 copyRightYear.forEach(year=>{
     const time = new Date()
@@ -16,4 +26,25 @@ document.addEventListener('click', () => {
     setTimeout(() => {
         cursor.classList.remove("expand");
     }, 500)
+})
+// get form data from local storage
+submitSignInData.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const formData={
+        name: nameUser.value,
+        email: email.value,
+        passcode: passcode.value,
+    }
+    if(formData.name === getFormData.name &&
+        formData.email === getFormData.email&&
+        formData.passcode === getFormData.passcode 
+     ){
+        window.location.href= "../blogs/AdminDashboard/ViewBlogs/blogs.html";
+        
+    }else{
+        window.location.href= "#";
+        alertMessage.style.display = "flex"
+        setTimeout(function () {alertMessage.style.display = "none"; }, 2000); 
+    }
+    // const 
 })
--- pages/SignUp/signUp.js
+++ pages/SignUp/signUp.js
@@ -1,4 +1,6 @@
 const copyRightYear = document.querySelectorAll(".year");
+const submitSignUpData = document.getElementById("submitSignUpData");
+const alertMessage = document.getElementById("alertMessage");
 
 copyRightYear.forEach(year=>{
     const time = new Date()
@@ -16,4 +18,50 @@ document.addEventListener('click', () => {
     setTimeout(() => {
         cursor.classList.remove("expand");
     }, 500)
+})
+// get form data
+submitSignUpData.addEventListener("click", (e)=>{
+    e.preventDefault()
+    const name = document.getElementById("name")
+    const email = document.getElementById("email")
+    const passcode = document.getElementById("passcode")
+    const passconfirm = document.getElementById("passconfirm")
+    const formData={
+        name: name.value,
+        email: email.value,
+        passcode: passcode.value,
+        confirmPass: passconfirm.value
+    }
+    if(formData.name &&
+        formData.email &&
+        formData.passcode &&
+        formData.confirmPass
+     ){
+        if( formData.passcode ===formData.confirmPass){
+            localStorage.setItem("SignedInSuccessfully", JSON.stringify(formData));
+            alertMessage.innerHTML="<p>Success!</p>"
+            alertMessage.style.display = "flex";
+            alertMessage.style.backgroundColor = "green";
+        setTimeout(function () {
+            alertMessage.style.display = "none";
+            alertMessage.style.backgroundColor = "red";
+            alertMessage.innerHTML="<p>Fill the missing fields plz!</p>"
+         }, 2000); 
+         window.location.href= "../SignIn/signIn.html";
+
+        }
+        else{
+        alertMessage.style.display = "flex";
+        alertMessage.innerHTML="<p>Passwords do not match!</p>"
+        setTimeout(function () {
+            alertMessage.style.display = "none";
+            alertMessage.innerHTML="<p>Fill the missing fields plz!</p>"
+         }, 2000);  
+        }
+    }else{
+        alertMessage.style.display = "flex"
+        setTimeout(function () {alertMessage.style.display = "none"; }, 2000);  
+
+    }
+    // const 
 })
