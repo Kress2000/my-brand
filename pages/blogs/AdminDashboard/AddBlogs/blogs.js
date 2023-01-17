87d167b37be562070082324cd94da907b7d0d6df
Kress2000 <erickykress1@gmail.com>
2023-01-17 05:20:52 +0200

    Functionality of add blogs pages

--- pages/blogs/AdminDashboard/AddBlogs/blogs.js
+++ pages/blogs/AdminDashboard/AddBlogs/blogs.js
@@ -1,5 +1,15 @@
 const copyRightYear = document.querySelectorAll(".year");
 const createBlogBtn = document.getElementById("createBtn");
+const uploadImg = document.getElementById("upload");
+const tv = document.getElementById("tv");
+const messageAlert = document.getElementById("messageAlert");
+const messageAlertSuccess = document.getElementById("messageAlertSuccess");
+// inputs form
+const title = document.getElementById("title");
+const category = document.getElementById("category");
+const details = document.getElementById("details");
+
+
 
 copyRightYear.forEach(year=>{
     const time = new Date()
@@ -18,7 +28,53 @@ document.addEventListener('click', () => {
     }, 500)
 })
 
-// submit data
+// get uploaded img
+let image = []
+let imgUrl = ""
+uploadImg.addEventListener("change", ()=>{
+    const filechosen=  uploadImg.files;
+    image = [filechosen[0]];
+    const displayImg = ()=>{
+        image.forEach((img) => {
+            const url = `url('${URL.createObjectURL(img)}')`
+            tv.style.backgroundImage = url;
+            tv.style.backgroundRepeat = "no-repeat";
+            imgUrl =URL.createObjectURL(img);
+        })
+    }
+    displayImg();
+})
+// submit data to local storage
+let blogDataArray=[]
 createBlogBtn.addEventListener("click", (e)=>{
     e.preventDefault();
+    const blogData={
+        title: title.value,
+        category: category.value,
+        details: details.value,
+        img: imgUrl
+    }
+    if(blogData.title &&
+        blogData.category &&
+        blogData.details &&
+        blogData.img 
+    ){
+        blogDataArray.push(blogData);
+        localStorage.setItem("blogDataAdd", JSON.stringify(blogDataArray));
+        messageAlertSuccess.style.display = "flex";
+        setTimeout(()=>{
+        messageAlertSuccess.style.display = "none";
+        // clean fields
+            title.value= "";
+            category.value= "";
+            details.value= "";
+            tv.style.backgroundImage = ""
+        }, 2000)
+    }else{
+        messageAlert.style.display = "flex"
+        setTimeout(()=>{
+            messageAlert.style.display = "none"
+        }, 2000)
+
+    }
 })
