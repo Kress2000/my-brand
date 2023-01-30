195fdee54e59e436dffa858dec1238441e1489ef
nsanzimfura eric <erickykress1@gmail.com>
2023-01-30 15:33:05 +0200

    sign up and sign in pages edit

--- pages/SignIn/signIn.js
+++ pages/SignIn/signIn.js
@@ -3,17 +3,11 @@ const submitSignInData = document.getElementById("submitSignUpData");
 const alertMessage = document.getElementById("alertMessage");
 const email = document.getElementById("email")
 const passcode = document.getElementById("passcode")
+//  users fron LS
 const getFormData = JSON.parse(localStorage.getItem("SignedInSuccessfully"));
 
-
-copyRightYear.forEach(year=>{
-    const time = new Date()
-    const timeYear = time.getFullYear();
-    year.innerText= timeYear;
-})
 // cursor
 const cursor = document.querySelector('.cursor');
-
 document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
 })
@@ -24,54 +18,57 @@ document.addEventListener('click', () => {
     }, 500)
 })
 
+const formData={
+    email: email.value,
+    passcode: passcode.value,
+}
+
 // get form data from local storage
 submitSignInData.addEventListener("click", (e)=>{
     e.preventDefault();
-    const formData={
-        email: email.value,
-        passcode: passcode.value,
-    }
     //admin dashboard
     if(formData.email.toLowerCase() === "erickykress1@gmail.com" &&
         formData.passcode.toLowerCase() === "kress123")
     {
-    window.location.href= "../blogs/AdminDashboard/ViewBlogs/blogs.html"; 
-        
+        window.location.href= "../blogs/AdminDashboard/ViewBlogs/blogs.html"; 
     }//user Daashboard
     else{
         //check if this email is already registered
-       const user =  getFormData.filter(user=>user.email.toLowerCase() === formData.email.toLowerCase() && user.passcode === formData.passcode?user:null)
-        if(user.length !==0){
-            // get him in 
-            localStorage.setItem("user", JSON.stringify(user)); //to be displayed in blogs dashboard
-            //getting user Locations
-    //         // if(JSON.parse(localStorage.getItem("locateUser"))===null){
-    //         //     localStorage.setItem("locateUser". JSON.stringify([]));
-    //         // }
-    //         // if('geolocation' in navigator ){
-    //         //     navigator.geolocation.getCurrentPosition(position=>{
-    //         //         let lng = position.coords.longitude;
-    //         //         let lat = position.coords.latitude;
-    //         //         let locateUser={
-    //         //             user: user,
-    //         //             location: {
-    //         //                 latitude: lat,
-    //         //                 longitude: lng
-    //         //             }
-    //         //         }
-    //         //         localStorage.setItem("locateUser". JSON.stringify(locateUser));
-    //         //         const locate = JSON.parse(localStorage.getItem("locateUser"))
-    //         //         alert(locate)
-                    
-    //         //     }, error=>{
-    //         //         alert(error.code)
-    //         //     })
-    // }
-            window.location.href= "../blogs/code/blogs.html";
-        }
-        else{ //edited credentials wrong, can't go in
-            alertMessage.style.display = "flex"
-            setTimeout(function () {alertMessage.style.display = "none"; }, 2000); 
-        }
+       getFormData.forEach(user=>{
+            if(user.email.toLowerCase() === formData.email.toLowerCase() && user.passcode === formData.passcode){
+                //getting user Locations
+                if('geolocation' in navigator ){
+                    navigator.geolocation.getCurrentPosition(position=>{
+                        let lng = position.coords.longitude;
+                        let lat = position.coords.latitude;
+                        let locateUser={
+                            user: user,
+                            location: {
+                                lat,
+                                lng
+                            }
+                        }
+                        localStorage.setItem("locateUser". JSON.stringify(locateUser));
+                        const locate = JSON.parse(localStorage.getItem("locateUser"))
+                        console.log(locate)
+                        
+                    }, error=>{
+                        alert(error.code);
+                    })
+                }
+                localStorage.setItem("user", JSON.stringify(user)); //to be displayed in blogs dashboard
+                window.location.href= "../blogs/code/blogs.html";
+            }else{//edited credentials wrong, can't go in
+                alertMessage.style.display = "flex"
+                setTimeout(function () {alertMessage.style.display = "none"; }, 2000); 
+            }
+       
+            
+        })
     }
+})
+copyRightYear.forEach(year=>{
+    const time = new Date()
+    const timeYear = time.getFullYear();
+    year.innerText= timeYear;
 })
--- pages/SignUp/signUp.js
+++ pages/SignUp/signUp.js
@@ -3,15 +3,18 @@ const submitSignUpData = document.getElementById("submitSignUpData");
 const alertMessage = document.getElementById("alertMessage");
 // when we have the email already
 const alreadyThere = document.getElementById("alreadyThere");
+// form data
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const passcode = document.getElementById("passcode");
+const confirmPass = document.getElementById("passconfirm");
+// reg exp
+const regexPatern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let regExName = /^[a-z\s/a-z]/gi;
 
-copyRightYear.forEach(year=>{
-    const time = new Date()
-    const timeYear = time.getFullYear();
-    year.innerText= timeYear;
-})
+
 // cursor
 const cursor = document.querySelector('.cursor');
-
 document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
 })
@@ -25,45 +28,49 @@ if(JSON.parse(localStorage.getItem("SignedInSuccessfully"))===null){
     localStorage.setItem("SignedInSuccessfully",JSON.stringify([]));
 }
 // get form data
+let users = JSON.parse(localStorage.getItem("SignedInSuccessfully")); //accesss LS
 submitSignUpData.addEventListener("click", (e)=>{
     e.preventDefault()
-    const name = document.getElementById("name")
-    const email = document.getElementById("email")
-    const passcode = document.getElementById("passcode")
-    const passconfirm = document.getElementById("passconfirm")
-    const regexPatern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    let regExName = /^[a-z\s/a-z]/gi;
     let formData={
-        name: name.value,
+        name: fullName.value,
         email: email.value,
         passcode: passcode.value,
-        confirmPass: passconfirm.value
+        confirmPass: confirmPass.value
     }
     if(formData.name &&
         formData.email &&
         formData.passcode &&
         formData.confirmPass
-     ){
-        if(formData.name.match(regExName) && formData.email.match(regexPatern)){ //check for name and email validity
+        ){
+            if(formData.name.match(regExName) && formData.email.match(regexPatern)){ //check for name and email validity
+                console.log(formData)
             // password must be more than 5 chars
             if(formData.passcode.length >=5){
                 // passwords must be equal
                 if(formData.passcode === formData.confirmPass){
-                    let users = JSON.parse(localStorage.getItem([]));
                     // when we have the email already
-                    if(users){
+                    if(users.length !==0){
                         users.forEach(user=>{
                             if(user.email.toLowerCase()===formData.email.toLowerCase()){
-                                // console.log(user.email, formData.email)
-                                formData =null
                                 alreadyThere.style.display = "flex";
                                 setTimeout(()=>{
                                     alreadyThere.style.display = "none";
                                 }, 2000);
+                            }else{
+                                users.push(formData);
+                                localStorage.setItem("SignedInSuccessfully", JSON.stringify(users));
+                                alertMessage.innerHTML="<p>Success!</p>"
+                                alertMessage.style.display = "flex";
+                                alertMessage.style.backgroundColor = "green";
+                                setTimeout(function () {
+                                    alertMessage.style.display = "none";
+                                    alertMessage.style.backgroundColor = "red";
+                                    alertMessage.innerHTML="<p>Fill the missing fields plz!</p>"
+                                }, 2000); 
+                                window.location.href= "../SignIn/signIn.html";
                             }
                         })
-                    }
-                    if(formData !==null){
+                    }else if(users.length ===0){
                         users.push(formData);
                         localStorage.setItem("SignedInSuccessfully", JSON.stringify(users));
                         alertMessage.innerHTML="<p>Success!</p>"
@@ -107,5 +114,10 @@ submitSignUpData.addEventListener("click", (e)=>{
         alertMessage.style.display = "flex"
         setTimeout(function () {alertMessage.style.display = "none"; }, 2000);  
     }
-    // const 
+})
+
+copyRightYear.forEach(year=>{
+    const time = new Date()
+    const timeYear = time.getFullYear();
+    year.innerText= timeYear;
 })
